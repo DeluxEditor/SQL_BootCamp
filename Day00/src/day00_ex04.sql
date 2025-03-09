aebcba7115e48dad04c759659f68ebd7992c1e72
Eduard <e.reyh@cepr.ru>
2025-03-09 13:49:31 +0700

    [upd] rewrite exercise 0-4 on true code

--- Day00/src/day00_ex04.sql
+++ Day00/src/day00_ex04.sql
@@ -1,7 +1,16 @@
-SELECT 
-p.name || ' (age:' || 
-p.age || ',gender:' || 
-p.gender || ',address:' 
-|| p.address || ')' 
-AS person_information 
-FROM "person" p ;
+-- SELECT p.name || ' (age:' || p.age || ',gender:''' || p.gender || ''',address:''' || p.address || ''')' AS person_information
+-- FROM "person" p
+-- ORDER BY person_information;
+
+SELECT CONCAT(
+        name,
+        ' (age:',
+        age,
+        ',gender:''',
+        gender,
+        ''',address:''',
+        address,
+        ''')'
+    ) AS person_information
+FROM person
+ORDER BY person_information ASC;
